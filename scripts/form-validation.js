eae3bcc1e13ac1d8c1aafdfa8fa9efe5666abf77
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-01-09 19:17:16 -0400

    Form validation

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -1,28 +1,16 @@
-(function () {
-    'use strict';
-
-    var forms = document.querySelectorAll('.needs-validation');
-
-    Array.from(forms).forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-                printFormData(form);
-            }
-
-            form.classList.add('was-validated');
-        }, false);
-    });
-
-    function printFormData(form) {
-        var formData = new FormData(form);
-
-        console.log("Form Data:");
-
-        formData.forEach(function (value, key) {
-            console.log(key + ':', value);
-        });
-    }
-
-})();
+(() => {
+    'use strict'
+  
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
